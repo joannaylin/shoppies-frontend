58636f05508e1807cfc7e1fe86fa4ae3f7c222c0
joannaylin <lin.joanna.y@gmail.com>
2020-09-01 22:48:35 -0500

    added search bar and fetch request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com/?apikey=937f2766&s=";
 
 function App() {
+  const [movie, setMovie] = useState("");
+  const [results, setResults] = useState([])
+
+  const handleChange = (e) => {
+    setMovie(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`${API_URL}${movie}`)
+      .then((resp) => resp.json())
+      .then((data) => setResults(data.Search));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search for a movie to nominate...</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={movie} onChange={handleChange}></input>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
